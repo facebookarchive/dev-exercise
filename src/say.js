b1d6d1a8d46f0f3e4b36d69400b7f21df1a25f42
Mohsin Ali <mohammadmohsin@gmail.com>
2017-02-24 22:42:28 +0500

    All tests passing

--- src/say.js
+++ src/say.js
@@ -1,3 +1,9 @@
-module.exports = function say () {
+var say = function (arg1) {
 
+    let concatArgs = function(arg2){
+        if( typeof arg1 == 'string' && typeof arg2 == 'string')
+            return arg1+' '+arg2;
+    }
+    return concatArgs;
 }
+module.exports = say
